a26e919e51989662af1f53b10df2dad642c38c1b
Dokyun Lim <im.koremp@gmail.com>
2024-04-27 20:09:38 +0900

    feat: add header to components

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import Navigation from './nav/navigation';
+
+export default function Header() {
+  return (
+    <header className="h-10 px-4 sticky w-full">
+      <Navigation />
+    </header>
+  )
+}
