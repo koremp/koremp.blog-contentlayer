7f17dfda15dfc4734e540f47192b3022b1c4e6ab
Dokyun Claire Lim <im.koremp@gmail.com>
2024-04-30 21:12:40 +0900

    feat: add base structure of index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
+import PageLayout from '@/src/components/page-layout';
+import Header from '../components/header';
+import Main from '../components/main';
+
 export default function Home() {
   return (
-    <div>
-      <div>
-        Navigation
-        <ul>
-          <li>about</li>
-          <li>posts</li>
-          <li>wiki</li>
-          <li>rewind</li>
-          <li>diary</li>
-        </ul>
-      </div>
-      <div>
-        <div>recent posts</div>
-        <div>recent wiki</div>
-        <div>recent rewind</div>
-        <div>recent diary</div>
-      </div>
-    </div>
+    <PageLayout>
+      <Header />
+      <Main />
+    </PageLayout>
   );
 }
